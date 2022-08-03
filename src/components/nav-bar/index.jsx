13e0e7e474ad61fb5ee5cedbac887f8d5fbf569a
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-08-03 16:37:02 -0400

    setting up a nav bar

---
+++ src/components/nav-bar/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Navbar= () => {
+
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>LOGO</h1>
+                </NavLink>
+                <Bars>
+
+                </Bars>
+                <NavMenu>
+                    <NavLink to="/about" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to="/services" activeStyle>
+                        services
+                    </NavLink>
+                    <NavLink to="/contact-us" activeStyle>
+                        contact
+                    </NavLink>
+                    <NavLink to="/sign-up" activeStyle>
+                        sign up
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/sign-up'> </NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar
